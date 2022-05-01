59d0c7579eef9d3bc16573aa83847c1d7193a892
toshikazu kitani <toshigas@gmail.com>
2022-05-01 20:21:07 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    println!("Hello, world!");
+    let mut handlers = Vec::new();
+    let data = Arc::new(Mutex::new(vec![1; 10]));
+
+    for x in 0..10 {
+        let data_ref = data.clone();
+        
+    handlers.push(thread::spawn(move || {
+            let mut data2 = data_ref.lock().unwrap();
+            data2[x] += 1; 
+        }));
+    }
+
+    for handler in handlers {
+        let _ = handler.join();
+    }
+
+    dbg!(data);
+
+     println!("Hello, world!");
 }
